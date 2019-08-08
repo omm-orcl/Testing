c70a66ccc4687dba58ecf5b020f44706c4101580
omprakash <omm.orcl@gmail.com>
2019-08-08 14:59:50 +0530

    this is my modified code php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>My php test</title>
  </head>
  <body>
- <?php echo '<p>hello world-1 this is my first build</p>'; ?>
+ <?php echo '<p>hello world-1 this is my 1st build</p>'; ?>
  </body>
 </html>
